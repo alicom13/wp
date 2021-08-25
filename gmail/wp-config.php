bb99b2181c5b1ef8c8e3af5f3506056649bdccd4
Ali Musthofa <alfaonline13@gmail.com>
2021-08-25 13:28:03 +0700

    Create wp-config.php

---
+++ gmail/wp-config.php
@@ -0,0 +1,10 @@
+// SMTP email settings
+define( 'SMTP_username', 'emailkamu@gmail.com' );  // username of host like Gmail
+define( 'SMTP_password', 'katasandinya-gmail-kamu' );   // password for login into the App
+define( 'SMTP_server', 'smtp.gmail.com' );     // SMTP server address
+define( 'SMTP_FROM', 'youremail@example.com' );   // Your Business Email Address
+define( 'SMTP_NAME', 'Web Bisnis' );   //  Business From Name
+define( 'SMTP_PORT', '587' );     // Server Port Number
+define( 'SMTP_SECURE', 'tls' );   // Encryption - ssl or tls
+define( 'SMTP_AUTH', true );  // Use SMTP authentication (true|false)
+define( 'SMTP_DEBUG',   0 );  // for debugging purposes only
