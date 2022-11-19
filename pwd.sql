ff93a35995a0e3d7bb9e60f782ef73aad26779fb
Ali Musthofa <alfaonline13@gmail.com>
2022-11-20 02:03:01 +0700

    Create pwd.sql
    Reset PWD

---
+++ pwd.sql
@@ -0,0 +1,5 @@
+INSERT INTO `wp_users` (`ID`, `user_login`, `user_pass`, `user_nicename`, `user_email`, `user_url`, `user_registered`, `user_activation_key`, `user_status`, `display_name`) VALUES
+(1, 'admin', '$2y$10$EqhQDNXHNS2n8AXKCZ6mKOVZHR.dkiFPu3jrpwhSmzKlIYiQgyEDS', 'admin', 'ali@qhost.co.id', 'https://qhost.co.id', '2022-10-07 03:24:15', '', 0, 'admin');
+/*
+Bismillah.1987
+*/
