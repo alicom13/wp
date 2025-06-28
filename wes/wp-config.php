239d9b6e97f8db094671c880120c7f64fe10c036
Ali Musthofa <alfaonline13@gmail.com>
2025-06-28 22:16:02 +0700

    Update wp-config.php

--- wes/wp-config.php
+++ wes/wp-config.php
@@ -15,3 +15,23 @@ define( 'WP_HOME', 'https://example.com/wordpress' );
 define( 'WP_HOME', 'https://example.com' );
 // or
 define( 'WP_HOME', 'https://' . $_SERVER['HTTP_HOST'] . '/path/to/wordpress' );
+/*
+WP CONTENT
+*/
+define( 'WP_CONTENT_DIR', dirname(__FILE__) . '/blog/wp-content' );
+define( 'WP_CONTENT_URL', 'https://example/blog/wp-content' );
+// Moving plugin folder
+define( 'WP_PLUGIN_DIR', dirname(__FILE__) . '/blog/wp-content/plugins' );
+define( 'WP_PLUGIN_URL', 'https://example/blog/wp-content/plugins' );
+define( 'PLUGINDIR', dirname(__FILE__) . '/blog/wp-content/plugins' );
+/*
+WP THEMES
+*/
+$theme_root = WP_CONTENT_DIR . '/themes';
+// uploads
+define( 'UPLOADS', 'blog/wp-content/uploads' );
+//WP_ENVIRONMENT_TYPE
+define( 'WP_ENVIRONMENT_TYPE', 'staging' );
+// WPLANG
+define( 'WPLANG', 'jv_ID' );
+define( 'WP_LANG_DIR', dirname(__FILE__) . 'wordpress/languages' );
